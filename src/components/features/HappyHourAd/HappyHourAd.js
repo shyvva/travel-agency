bad2dd7b091547174f97e3b6df27c072f2c4cc85
Dawid Przydryga <shivva90@gmail.com>
2020-06-22 17:00:59 +0200

    Finish HappyHourAd component, add tests

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import styles from './HappyHourAd.scss';
+import styles from './HappyHourAd.scss';
 
 
 
 class HappyHourAd extends React.Component {
+  constructor(){
+    super();
+
+    /* run this.forceUpdate() every second */
+    setInterval(() => this.forceUpdate(), 1000);
+  }
+
   getCountdownTime(){
     const currentTime = new Date();
     const nextNoon = new Date(Date.UTC(currentTime.getUTCFullYear(), currentTime.getUTCMonth(), currentTime.getUTCDate(), 12, 0, 0, 0));
@@ -15,12 +22,21 @@ class HappyHourAd extends React.Component {
 
     return Math.round((nextNoon.getTime() - currentTime.getTime())/1000);
   }
+  static defaultProps = {
+    title: 'Happy Hour',
+    description:
+      'Its your time! Take advantage of Happy Hour! All offers 20% off!',
+  };
   render() {
-    const { title } = this.props;
+    const {title, description} = this.props;
+    const countdownTime = this.getCountdownTime();
     return (
-      <div>
-        <h3 className="title">{title}</h3>
-        <div className='promoDescription'>{this.getCountdownTime()}</div>
+      <div className={styles.component}>
+        <h3 className={styles.title}>{title}</h3>
+        {countdownTime > 23*60*60 ?
+          <div className={styles.promoDescription}>{description}</div>
+          :
+          <div className={styles.promoDescription}>{this.getCountdownTime()}</div>}
       </div>
     );
   }
